2d76aa990919271a6e79ef7e259d06e00a6cf8c4
Trang Nguyen <ntrang086@gmail.com>
2018-01-08 13:58:03 -0500

    style: Formatting

--- 01_defeat_learners/LinRegLearner.py
+++ 01_defeat_learners/LinRegLearner.py
@@ -1,4 +1,4 @@
-"""TODO A simple wrapper for Decision Tree regression"""
+"""A simple wrapper for linear regression"""
 
 import numpy as np
 
@@ -15,20 +15,20 @@ class LinRegLearner(object):
         """
 
         # Add 1s column so linear regression finds a constant term
-        newdataX = np.ones([dataX.shape[0],dataX.shape[1]+1])
+        newdataX = np.ones([dataX.shape[0], dataX.shape[1] + 1])
         newdataX[:,0:dataX.shape[1]] = dataX
 
         # build and save the model
         self.model_coefs, residuals, rank, s = np.linalg.lstsq(newdataX, dataY)
         
     def query(self, points):
-        """Estimate a set of test points given the model we built.
+        """Estimate a set of test points given the model we built
         Parameters:
-        points: should be a numpy array with each row corresponding to a specific query.
+        points: A numpy array with each row corresponding to a specific query
 
-        Returns: the estimated values according to the saved model.
+        Returns: the estimated values according to the saved model
         """
         return (self.model_coefs[:-1] * points).sum(axis=1) + self.model_coefs[-1]
 
 if __name__=="__main__":
-    print ("the secret clue is 'zzyzx'")
+    print ("This is a Linear Regression Learner")
