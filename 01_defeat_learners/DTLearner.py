61ad47b12de1e2e878d829f833ed811e559dba95
Trang Nguyen <ntrang086@gmail.com>
2018-01-10 18:11:39 -0500

    test: Test DTLearner with some data

--- 01_defeat_learners/DTLearner.py
+++ 01_defeat_learners/DTLearner.py
@@ -158,3 +158,30 @@ class DTLearner(object):
 
 if __name__=="__main__":
     print ("This is a Decision Tree Learner")
+
+    # Some data to test the DTLearner
+    x0 = np.array([0.885, 0.725, 0.560, 0.735, 0.610, 0.260, 0.500, 0.320])
+    x1 = np.array([0.330, 0.390, 0.500, 0.570, 0.630, 0.630, 0.680, 0.780])
+    x2 = np.array([9.100, 10.900, 9.400, 9.800, 8.400, 11.800, 10.500, 10.000])
+    x = np.array([x0, x1, x2]).T
+    
+    y = np.array([4.000, 5.000, 6.000, 5.000, 3.000, 8.000, 7.000, 6.000])
+
+    # Create a tree learner from given train X and y
+    dtl = DTLearner(verbose=True)
+    dtl.addEvidence(x, y)
+
+    # Create a tree learner from an existing tree
+    dtl2 = DTLearner(tree=dtl.tree)
+
+    # dtl2 should have the same tree as dtl
+    assert np.any(dtl.tree == dtl2.tree)
+
+    dtl2.get_learner_info()
+
+    # Modify the dtl2.tree and assert that this doesn't affect dtl.tree
+    dtl2.tree[0] = np.arange(dtl2.tree.shape[1])
+    assert np.any(dtl.tree != dtl2.tree)
+
+    # Querry data with dummy data
+    dtl.query(np.array([[1, 2, 3], [0.2, 12, 12]]))
